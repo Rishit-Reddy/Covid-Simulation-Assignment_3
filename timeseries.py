281e6d8ca471be921d513389ec8f68d2d89fea88
Rishit Reddy <80318887+Rishit-Reddy@users.noreply.github.com>
2023-10-04 12:01:13 +0200

    Update timeseries.py

--- timeseries.py
+++ timeseries.py
@@ -1,31 +1,5 @@
 
 import pandas as pd
-import random
-
-def create_sample_dataframe():
-    # Initialize an empty DataFrame
-    sample_df = pd.DataFrame(columns=['person_id', 'age_group_name', 'country'])
-
-    # Define the possible values for age_group_name and country
-    age_groups = ["less_5", "5_to_14", "15_to_24", "25_to_64", "over_65"]
-    countries = ["S", "A", "J"]
-
-    # Generate 50 random records
-    for i in range(167):
-        person_id = 0  # Unique person_id for each record
-        age_group = random.choice(age_groups)  # Randomly select an age group
-        country = random.choice(countries)  # Randomly select a country
-
-        # Create a temporary DataFrame for the current record
-        record_df = pd.DataFrame({'person_id': [person_id], 'age_group_name': [age_group], 'country': [country]})
-
-        # Concatenate the temporary DataFrame with the sample_df
-        sample_df = pd.concat([sample_df, record_df], ignore_index=True)
-
-    return sample_df
-
-# Create the sample DataFrame
-sample_dataframe = create_sample_dataframe()
 
 def create_timeseries(sample_df, start_date, end_date):
     # Create an empty DataFrame with the desired columns
@@ -44,16 +18,3 @@ def create_timeseries(sample_df, start_date, end_date):
     timeseries_df['staying_days'] = ''
 
     return timeseries_df
-
-# Create the sample DataFrame
-sample_dataframe = create_sample_dataframe()
-
-# Define the start and end dates
-start_date = '2021-04-01'
-end_date = '2022-04-30'
-
-# Create the time-series DataFrame
-timeseries_dataframe = create_timeseries(sample_dataframe, start_date, end_date)
-
-# Print the first few records to check
-print(timeseries_dataframe)
